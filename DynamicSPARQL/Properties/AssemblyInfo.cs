3607ccd50df76ce55577d88d0feaa1448efd997b
Efimster <efimster@gmail.com>
2013-11-15 14:15:38 +0100

    changes project properties; add readme.txt

--- DynamicSPARQL/Properties/AssemblyInfo.cs
+++ DynamicSPARQL/Properties/AssemblyInfo.cs
@@ -6,12 +6,12 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("DynamicSPARQL")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Helps making SPARQ queries to RDF sources")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("SITRONICS TS")]
+[assembly: AssemblyCompany("Efim Goncharuk")]
 [assembly: AssemblyProduct("DynamicSPARQL")]
-[assembly: AssemblyCopyright("Copyright © SITRONICS TS 2013")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCopyright("Efimster")]
+[assembly: AssemblyTrademark("Copyright 2013 (c) Efimster")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
