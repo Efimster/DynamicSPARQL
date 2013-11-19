83209b19f873df2b87a837616a03318d66b7cae8
Efimster <efimster@gmail.com>
2013-11-19 15:28:24 +0100

    add BrightstarDB connector; Tests;

---
+++ DynamicSPARQL.BrightstarDB.Tests/Connector.Fixture.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BrightstarDB.Client;
+using DynamicSPARQLSpace;
+using Xunit;
+using Should.Fluent;
+
+namespace DynamicSPARQLSpace.BrightstarDB.Tests
+{
+    public class ConnectoreFixture
+    {
+        private IBrightstarService client;
+
+        [Fact(DisplayName = "Brightstar Connector Test")]
+        public void BrightstarConnectorTest()
+        {
+            string storeName = null;
+            
+            try
+            {
+                storeName = GenerateTestStore();
+                var func = Connector.GetQueringFunction("type=embedded;storesdirectory=brightstar;storename=" + storeName);
+
+                var dyno = DynamicSPARQL.CreateDyno(func, autoquotation: true);
+
+                IEnumerable<dynamic> list = dyno.Select(
+                        projection: "?s ?p ?o",
+                        where: SPARQL.Group(SPARQL.Tripple("?s ?p ?o"))
+                );
+
+                IList<string> result = list.Select(triple => (string)triple.o).ToList();
+
+                result.Should().Contain.One("nosql");
+                result.Should().Contain.One(".net");
+                result.Should().Contain.One("rdf");
+            }
+            finally
+            {
+                if (client!=null && storeName!=null)
+                    client.DeleteStore(storeName);
+            }
+        }
+
+        private string GenerateTestStore()
+        {
+            client = BrightstarService.GetClient("type=embedded;storesdirectory=brightstar;");
+            string storeName = "Store_" + Guid.NewGuid();
+            client.CreateStore(storeName);
+            
+            var data = new StringBuilder();
+            data.AppendLine("<http://www.brightstardb.com/products/brightstar> <http://www.brightstardb.com/schemas/product/name> \"BrightstarDB\" .");
+            data.AppendLine("<http://www.brightstardb.com/products/brightstar> <http://www.brightstardb.com/schemas/product/category> <http://www.brightstardb.com/categories/nosql> .");
+            data.AppendLine("<http://www.brightstardb.com/products/brightstar> <http://www.brightstardb.com/schemas/product/category> <http://www.brightstardb.com/categories/.net> .");
+            data.AppendLine("<http://www.brightstardb.com/products/brightstar> <http://www.brightstardb.com/schemas/product/category> <http://www.brightstardb.com/categories/rdf> .");
+
+            client.ExecuteTransaction(storeName, null, null, data.ToString());
+
+
+            return storeName;
+        }
+    }
+}
---
+++ DynamicSPARQL.BrightstarDB/Connector.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+using BrightstarDB;
+using BrightstarDB.Client;
+using VDS.RDF.Query;
+using System.Text.RegularExpressions;
+using HelperExtensionsLibrary.IEnumerable;
+
+namespace DynamicSPARQLSpace.BrightstarDB
+{
+    public static class Connector
+    {
+        public static Func<string, SparqlResultSet> GetQueringFunction(string connectionString)
+        {
+            var client = BrightstarService.GetClient(connectionString);
+            string storeName = Regex.Match(connectionString, @"storename=[\w-]+").Value.Split('=')[1];
+
+            Func<string, SparqlResultSet> queringFunction = xquery =>
+            {
+                var settings = new XmlReaderSettings();
+                settings.ConformanceLevel = ConformanceLevel.Auto;
+                settings.IgnoreComments = true;
+                settings.IgnoreWhitespace = true;
+
+                XmlDocument doc = new XmlDocument();
+                var reader = XmlReader.Create(client.ExecuteQuery(storeName, xquery, resultsFormat: SparqlResultsFormat.Xml), settings);
+                var root = XElement.Load(reader, LoadOptions.None);
+                foreach (XElement XE in root.DescendantsAndSelf())
+                {
+                    XE.Name = XE.Name.LocalName;
+                    XE.ReplaceAttributes((from xattrib in XE.Attributes().Where(xa => !xa.IsNamespaceDeclaration) select new XAttribute(xattrib.Name.LocalName, xattrib.Value)));
+                }
+                var sp = root.Elements().Where(x => x.Name.LocalName == "head").First();
+                sp.Elements().ForEach(el =>
+                {
+                    var name = el.Attribute("name").Value;
+                    el.RemoveAttributes();
+                    el.Value = name;
+                });
+
+                sp.ReplaceWith(new XElement("variables", sp.Elements()));
+
+                var settings2 = new XmlWriterSettings();
+                settings2.OmitXmlDeclaration = true;
+                settings2.NamespaceHandling = NamespaceHandling.OmitDuplicates;
+                settings2.ConformanceLevel = ConformanceLevel.Fragment;
+
+                var mem = new MemoryStream();
+                var writer = XmlWriter.Create(mem, settings2);
+
+                root.Elements().ForEach(el => el.WriteTo(writer));
+                writer.Dispose();
+                mem.Seek(0, SeekOrigin.Begin);
+
+                var result = new SparqlResultSet();
+                result.ReadXml(XmlReader.Create(mem, settings));
+
+                return result;
+            };
+
+            return queringFunction;
+
+        }
+
+    }
+}
